b74ee2bd3902841eba47bb2876806a8238603a90
EKo Sup <e.supardjo@gmx.de>
2019-06-06 15:01:48 +0200

    added dependency

--- app/src/main/java/com/example/ue_3/GraphView1.java
+++ app/src/main/java/com/example/ue_3/GraphView1.java
@@ -3,16 +3,13 @@ package com.example.ue_3;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+
 
 public class GraphView1 extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        float[] values = new float[] { "your values"};
-        String[] verlabels = new String[] { "your values" };
-        String[] horlabels = new String[] { "your values"
-                GraphView graphView = new GraphView(this, values,"GraphView",horlabels, verlabels, GraphView.BAR);
-        setContentView(graphView);
         }
 }
