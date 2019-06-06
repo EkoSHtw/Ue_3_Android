8b8aec02edbc42bc07ba20bf74bb7bb77f91813c
EKo Sup <e.supardjo@gmx.de>
2019-06-06 15:22:30 +0200

    remove error

--- app/src/main/java/com/example/ue_3/GraphView1.java
+++ app/src/main/java/com/example/ue_3/GraphView1.java
@@ -11,5 +11,12 @@ public class GraphView1 extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //change to actual values
+        float[] values = new float[] { 0,1f};
+        String[] verlabels = new String[] { "your values" };
+        String[] horlabels = new String[] { "your values"};
+
+        GraphView graphView = new GraphView(this);
+        setContentView(graphView);
         }
 }
